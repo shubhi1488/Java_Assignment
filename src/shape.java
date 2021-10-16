7cc610ff84a97af3a8c58dc3e60a0848e35409a6
HP <shubhi1488@gmail.com>
2021-10-16 11:31:13 +0530

    method

---
+++ src/shape.java
@@ -0,0 +1,14 @@
+public class shape {
+    static void draw(){
+        System.out.println("Drawing shape");
+    }
+    static void erase(){
+        System.out.println("Erasing shape");
+    }
+
+    public static void main(String[] args) {
+        draw();
+        erase();
+
+    }
+}

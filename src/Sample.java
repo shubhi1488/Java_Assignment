3045b0a6468a86c2a6d34c54428974f8c54ec04e
HP <shubhi1488@gmail.com>
2021-10-16 10:57:58 +0530

    String

---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
+        String name= sc.nextLine();
+        System.out.println("Welcome"+name);
+    }
+}
